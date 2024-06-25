814290a2cbf875923ecbdba9db76b87eea1fbb9f
sarankumar22004 <ellithoraiganesh@gmail.com>
2024-06-25 23:06:03 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+var geographiclocation = "ooty";
+ var jobtitle= "web develpoer at ";
+ console.log(jobtitle,geographiclocation);
+
+
+ //On a single line, declare three variables with the given names and values: firstName = "Ram" lastName = "Kumar" age = 30.
+
+ var firstName="saran", lastName="kumar",age=30;
+ console.log(firstName,lastName,age);
+
+//  Age Calculator
+let birthYear = 2004;
+let futureYear = 2025;
+let currentAge = futureYear - birthYear;
+let nextAge = currentAge + 1;
+console.log(`I will be either ${currentAge} or ${nextAge} in ${futureYear}`);
+
+//Ever wonder how much a "lifetime supply" of your favorite snack is? Wonder no more!
+let currentage = 30; 
+let maximumAge = 80; 
+let amountPerDay = 2;
+let remainingYears = maximumAge - currentage;
+let snacksPerYear = amountPerDay * 365; 
+let totalSnacksNeeded = snacksPerYear * remainingYears;
+console.log(`You will need ${totalSnacksNeeded} snacks to last you until the ripe old age of ${maximumAge}.`);
+
